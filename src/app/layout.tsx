fc80a76bd0b468e5cda5d903eff0a6c82ca19ccd
Samuel David Castro Mercado <sdavidcastro06@gmail.com>
2025-05-04 12:59:32 +0000

    Ahora quita la opcion de cambiar de tema entre claro y oscuro, que solo este como opcion el oscuro y sea el por defecto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
-import { ThemeProvider } from "@/components/theme-provider"; // Import ThemeProvider
-import { ThemeToggle } from "@/components/theme-toggle"; // Import ThemeToggle
 
 export const metadata: Metadata = {
   title: 'Alumbra',
@@ -18,20 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     // Ensure no whitespace between <html> and <body> tags
-    <html lang="es" className={`${GeistSans.variable} ${GeistMono.variable}`} suppressHydrationWarning><body className={`antialiased`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <div className="fixed bottom-4 right-4 z-50"> {/* Position ThemeToggle */}
-            <ThemeToggle />
-          </div>
+    <html lang="es" className={`${GeistSans.variable} ${GeistMono.variable}`} suppressHydrationWarning><body className={`antialiased dark`}>
           {children}
           <Toaster />
-        </ThemeProvider>
       </body>
     </html>
   );
 }
+
